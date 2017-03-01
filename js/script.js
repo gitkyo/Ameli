6912e5b679306cc22051ac1b9a908500df39a88f
gitkyo <p.delaunay1@gmail.com>
2017-03-01 19:26:41 +0100

    nouvelle itération

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+Custom script  
+*/
+jQuery( document ).ready(function() {
+
+	window.init = function() {
+
+		//if( $('.container').hasClass('infoSalarie') ) initInfoSaisie();
+
+
+		
+	}
+	
+	init(); // true	
+});
