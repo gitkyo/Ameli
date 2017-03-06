b5682f379f15d4981c865c1ba9ea7cb777803fa5
gitkyo <p.delaunay1@gmail.com>
2017-03-06 22:12:24 +0100

    finalisation anciennes maquettes + debut identification

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 /*
-Custom script  
+Custom script
 */
 jQuery( document ).ready(function() {
 
+	var initInfoConnexion = function(){
+		$('.loginBlock button').click(function(event) {
+			/* Act on the event */
+			$('div.loginRow p').addClass('hidden');
+			$('div.hiddenForm').removeClass('hidden');
+
+		});
+	}
+
 	window.init = function() {
 
-		//if( $('.container').hasClass('infoSalarie') ) initInfoSaisie();
+		if( $('.container-fluid.main').hasClass('connexion') ) initInfoConnexion();
 
-
-		
 	}
-	
-	init(); // true	
+
+	init(); // true
 });
