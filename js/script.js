b8de66fcbeb66e64c58679e6b46ffd314862c085
Pier <pierre_delaunay@natural-solutions.eu>
2017-03-07 18:19:47 +0100

    avance connexion employeur

--- js/script.js
+++ js/script.js
@@ -8,13 +8,22 @@ jQuery( document ).ready(function() {
 			/* Act on the event */
 			$('div.loginRow p').addClass('hidden');
 			$('div.hiddenForm').removeClass('hidden');
-
 		});
+	}
+
+	var initIdentification = function(){
+
+		//init custom select
+		$('select').selectric();
+		//init datePicker
+		$('#datetimepicker1').datetimepicker();
+
 	}
 
 	window.init = function() {
 
 		if( $('.container-fluid.main').hasClass('connexion') ) initInfoConnexion();
+		if( $('.container-fluid.main').hasClass('identification') ) initIdentification();
 
 	}
 
