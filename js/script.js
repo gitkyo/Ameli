cc905965798544e04270b4b771d0d636cb7540de
gitkyo <p.delaunay1@gmail.com>
2017-03-10 01:26:22 +0100

    JavaScript dynamique champs dates

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ jQuery( document ).ready(function() {
 		$('[data-toggle="tooltip"]').tooltip();
 		//init custom select
 		$('select').selectric();
-		//init datePicker
+		//init dateTimePicker
 		$('#datetimepicker1').datetimepicker({
 			format: 'DD/MM/YYYY'
 		});
@@ -47,11 +47,25 @@ jQuery( document ).ready(function() {
 			format: 'DD/MM/YYYY',
 			useCurrent: false //Important! See issue #1075
 		});
+		$('#datetimepicker4').datetimepicker({
+			format: 'DD/MM/YYYY'
+		});
+		$('#datetimepicker5').datetimepicker({
+			format: 'DD/MM/YYYY',
+			useCurrent: false //Important! See issue #1075
+		});
+		// Link dateTimePicker bettween them
 		$("#datetimepicker2").on("dp.change", function (e) {
 			$('#datetimepicker3').data("DateTimePicker").minDate(e.date);
 		});
 		$("#datetimepicker3").on("dp.change", function (e) {
 			$('#datetimepicker2').data("DateTimePicker").maxDate(e.date);
+		});
+		$("#datetimepicker4").on("dp.change", function (e) {
+			$('#datetimepicker5').data("DateTimePicker").minDate(e.date);
+		});
+		$("#datetimepicker5").on("dp.change", function (e) {
+			$('#datetimepicker4').data("DateTimePicker").maxDate(e.date);
 		});
 
 		/*
@@ -59,6 +73,125 @@ jQuery( document ).ready(function() {
 		$(window).bind('beforeunload', function(){
 		  return "Vous êtes sur le point de quitter le questionnaire avant de l'avoir finalisé. Vous pourrez le reprendre en l'état en vous connectant à nouveau au service avec votre code d'accès.";
 		});*/
+
+		// Get DOM of block "nouvelle absence"
+		var oneBlock = $("div.rightZone div.oneBlock").html();
+		// some counter
+		var cpt = 1;
+		var cpt1DateTimePicker = 4;
+		var cpt2DateTimePicker = 5;
+		var cptInput1 = 14;
+		var cptInput2 = 15;
+		var cptInput3 = 40;
+		//When click to add new absence
+		$("span.separation p").click(function(event) {
+			/* Act on the event */
+			cpt++;
+			cptInput1++;
+			cptInput2++;
+			cptInput3++;
+
+			// Add DOM block "nouvelle absence"
+			$("span.separation").before("<div class='oneBlock'>" + oneBlock + '</div>');
+
+
+			var lastBlock = $("div.rightZone div.oneBlock:last-of-type");
+
+
+			lastBlock.find("p").text("Absence " + cpt);
+
+			// Alter some id to init new dateTimePicker
+			/*$("div.rightZone div.oneBlock:last-of-type #datetimepicker"+(cpt1DateTimePicker - 1)).attr("id", "datetimepicker"+(cpt1DateTimePicker + 1));
+			$("div.rightZone div.oneBlock:last-of-type #datetimepicker"+(cpt2DateTimePicker - 1)).attr("id", "datetimepicker"+(cpt2DateTimePicker + 1));*/
+			/*cpt1DateTimePicker++;
+			cpt2DateTimePicker++;*/
+
+
+			if(cpt > 2){
+				/*console.log('là : '+cpt);
+				console.log("new datetimepicker"+(cpt1DateTimePicker + cpt));
+				console.log("new datetimepicker"+(cpt2DateTimePicker + cpt));
+				console.log("-----");*/
+
+				lastBlock.find(".first .date").attr("id", "datetimepicker"+(cpt1DateTimePicker + cpt));
+				lastBlock.find(".second .date").attr("id", "datetimepicker"+(cpt2DateTimePicker + cpt));
+			}else{
+
+				/*console.log("#datetimepicker"+(cpt1DateTimePicker));
+				console.log("#Changer en");
+				console.log("datetimepicker"+(cpt1DateTimePicker + 2));
+				console.log("et");
+				console.log("#datetimepicker"+(cpt2DateTimePicker));
+				console.log("#Changer en");
+				console.log("datetimepicker"+(cpt2DateTimePicker + 2));
+				console.log("-----");*/
+
+				lastBlock.find("#datetimepicker"+(cpt1DateTimePicker)).attr("id", "datetimepicker"+(cpt1DateTimePicker + 2));
+				lastBlock.find("#datetimepicker"+(cpt2DateTimePicker)).attr("id", "datetimepicker"+(cpt2DateTimePicker + 2));
+			}
+
+			cpt1DateTimePicker++;
+			cpt2DateTimePicker++;
+
+			// Alter for control ID
+			lastBlock.find("div.first label").attr("for", "input"+(cptInput1 + 1));
+			lastBlock.find("div.second label").attr("for", "input"+(cptInput2 + 1));
+
+			lastBlock.find("div.first input").attr("id", "input"+(cptInput1 + 1));
+			lastBlock.find("div.second input").attr("id", "input"+(cptInput2 + 1));
+
+			cptInput1++;
+			cptInput2++;
+
+
+			if(cpt > 2){
+				/*console.log("là");
+				console.log("#datetimepicker"+(cpt1DateTimePicker + cpt - 1));
+				console.log("#datetimepicker"+(cpt2DateTimePicker + cpt - 1));
+				console.log("---");*/
+
+				// Init new dateTimePicker
+				$("#datetimepicker"+(cpt1DateTimePicker + cpt - 1)).datetimepicker({
+					format: 'DD/MM/YYYY'
+				});
+				$("#datetimepicker"+(cpt2DateTimePicker + cpt - 1)).datetimepicker({
+					format: 'DD/MM/YYYY',
+					useCurrent: false //Important! See issue #1075
+				});
+
+				// Link new dateTimePicker bettween them
+				$("#datetimepicker"+(cpt1DateTimePicker + cpt - 1)).on("dp.change", function (e) {
+					$('#datetimepicker'+(cpt2DateTimePicker + cpt - 1)).data("DateTimePicker").minDate(e.date);
+				});
+				$("#datetimepicker"+(cpt2DateTimePicker + cpt - 1)).on("dp.change", function (e) {
+					$('#datetimepicker'+(cpt1DateTimePicker + cpt - 1)).data("DateTimePicker").maxDate(e.date);
+				});
+			}
+			else{
+				/*console.log("#datetimepicker"+(cpt1DateTimePicker + 1));
+				console.log("#datetimepicker"+(cpt2DateTimePicker + 1));
+				console.log("---");*/
+
+				// Init new dateTimePicker
+				$("#datetimepicker"+(cpt1DateTimePicker + 1)).datetimepicker({
+					format: 'DD/MM/YYYY'
+				});
+				$("#datetimepicker"+(cpt2DateTimePicker + 1)).datetimepicker({
+					format: 'DD/MM/YYYY',
+					useCurrent: false //Important! See issue #1075
+				});
+
+				// Link new dateTimePicker bettween them
+				$("#datetimepicker"+(cpt1DateTimePicker + 1)).on("dp.change", function (e) {
+					$('#datetimepicker'+(cpt2DateTimePicker + 1)).data("DateTimePicker").minDate(e.date);
+				});
+				$("#datetimepicker"+(cpt2DateTimePicker + 1)).on("dp.change", function (e) {
+					$('#datetimepicker'+(cpt1DateTimePicker + 1)).data("DateTimePicker").maxDate(e.date);
+				});
+			}
+
+			oneBlock= lastBlock.html();
+		});
 
 	}
 
