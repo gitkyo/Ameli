65844e84d940a1eaa9505b3b322c5f57394d3c0f
gitkyo <p.delaunay1@gmail.com>
2017-03-15 21:08:29 +0100

    correc css

--- js/script.js
+++ js/script.js
@@ -742,7 +742,7 @@ jQuery( document ).ready(function() {
 			cpt++;
 
 			var htmlRaisonSocial = '<div class="form-group mrgtop padBot">'+
-	                            		'<label for="input5" class="col-sm-4 control-label readOnly">Raison social de l’employeur '+cpt+' :</label>'+
+	                            		'<label for="input5" class="col-sm-4 control-label readOnly spec">Raison social de l’employeur '+cpt+' :</label>'+
 	                            		'<div class="col-sm-8">'+
 	                              		'<input type="text" class="form-control" id="input5" placeholder="">'+
 	                            		'</div>'+
