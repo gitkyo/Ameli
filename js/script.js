3724424549f8b0a149c539b178fb6d3ca5fdd94e
gitkyo <p.delaunay1@gmail.com>
2017-04-08 20:15:44 +0200

    premières modifs page pathologie

--- js/script.js
+++ js/script.js
@@ -369,15 +369,6 @@ jQuery( document ).ready(function() {
 		$('#datetimepicker1').datetimepicker({
 			format: 'DD/MM/YYYY'
 		});
-		$('#datetimepicker2').datetimepicker({
-			format: 'DD/MM/YYYY'
-		});
-		$('#datetimepicker3').datetimepicker({
-			format: 'DD/MM/YYYY'
-		});
-		$('#datetimepicker4').datetimepicker({
-			format: 'DD/MM/YYYY'
-		});
 
 		$('form').submit(function(event){
 			event.preventDefault();
